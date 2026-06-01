c6fff60fd06b801e6b4a7223c5c6cc62e952e8a7
Oyimafu Emmanuel <106592024+oyimafu@users.noreply.github.com>
2026-06-01 14:22:16 +0400

    Add root outputs

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "management_group_ids" {
+  value = module.management_groups.management_group_ids
+}
+
+output "management_group_display_names" {
+  value = module.management_groups.management_group_display_names
+}
